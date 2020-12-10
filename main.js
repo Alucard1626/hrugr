492587fc19ed3d6ccdcd9e87d26e91c48ec45582
JoelFreakOfficial <64692941+JoelFreakOfficial@users.noreply.github.com>
2020-12-10 09:58:10 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let character = document.getElementById("character");
+let game = document.getElementById("game");
+let interval;
+let both = 0;
+
+
+function moveLeft() {
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+    if(left>0){
+    character.style.left = left - 2 + "px";
+    }
+}
+
+function moveRight() {
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
+    if(left<780)
+    character.style.left = left + 2 + "px";
+
+}
+
+document.addEventListener("keydown", event => {
+    if(both==0);
+    {
+         both++;
+     
+     if(event.key==="ArrowLeft") {
+        interval = setInterval(moveLeft, 1);
+     }
+
+     if(event.key==="ArrowRight") {
+     interval = setInterval(moveRight, 1);
+     }
+
+
+}
+
+function character2 (character2){
+    
+}
+
+})
