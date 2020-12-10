e61ce458a471fca29540a7b64016fd673b66b8d1
JoelFreakOfficial <64692941+JoelFreakOfficial@users.noreply.github.com>
2020-12-10 15:56:51 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-var target = document.getElementById('target');
-var targetBox = document.getElementById('target-box');
+var container = document.querySelector("#contentContainer");
+var myCircle = document.querySelector("#circle");
 
-targetBox.addEventListener('click', mouseClicked);
+container.addEventListener("click", getClickPosition, false);
+function getClickPosition(e) {
+  var parentPosition = getPosition(container);
+  var xPosition = e.clientX - parentPosition.x - (myCircle.offsetWidth / 2);
+  var yPosition = e.clientY - parentPosition.y - (myCircle.offsetHeight / 2);
+  var translate3dValue = "translate3d(" + xPosition + "px," + yPosition + "px, 0px)";
+  myCircle.style.transform  = translate3dValue;
+}
 
-function mouseClicked (event){
-  var xposition = (event.clientX - target.offsetLeft - target.offsetWidth/2);
-  var yposition = (event.clientY - target.offsetTop - target.offsetHeight/2);
-  target.style.transform = "translate("+ xposition + "px," + yposition + "px)";
-  console.log(event);
+function getPosition(element){
+  var xPosition = 0;
+  var yPosition = 0;
+  
+  while(element){
+    xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+    yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+    element = element.offsetParent;
+  }
+  return{
+    x: xPosition,
+    y: yPosition
+  };
 }
