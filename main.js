90d327e39588c3028cb178e2e48373a3ae747c7c
JoelFreakOfficial <64692941+JoelFreakOfficial@users.noreply.github.com>
2020-12-10 16:21:31 +0000

    Update main.js

--- main.js
+++ main.js
@@ -16,13 +16,7 @@ function getClickPosition(e){
 function getPosition(element) {
   var xPosition = 0;
   var yPosition = 0;
- 
-  while (element) {
-      xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
-      yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
-      element = element.offsetParent;
-    }
-  
+
   return {
     x: xPosition,
     y: yPosition
