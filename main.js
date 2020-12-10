7925171fcf0e2eff73345ad880a49db159a2d6f0
JoelFreakOfficial <64692941+JoelFreakOfficial@users.noreply.github.com>
2020-12-10 15:38:30 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+let thePenguin = document.querySelector("#penguin")
+let container = document.querySelector("#contentContainer")
 
+container.addEventListener("click", getClickPosition, false)
+
+function getClickPosition(e) {
+  let xPosition = e.clientX;
+  let yPosition = e.clientY;
+ 
+  let translate3dValue = "translate3d(" + xPosition + "px" + yPosition + "px, 0")
+  thePenguin.style.transform = translate3dValue;
+}
