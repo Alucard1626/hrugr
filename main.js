095c93799164cf9fab9ff961d3477a210112e8f1
JoelFreakOfficial <64692941+JoelFreakOfficial@users.noreply.github.com>
2020-12-10 13:06:15 +0000

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function moveLeft() {
 
 function moveRight() {
     var left = parseInt(window.getComputedStyle(character).getPropertyValue('left'));
-    if(left<700)
+    if(left<690)
     character.style.left = left + 2 + "px";
 
 }
